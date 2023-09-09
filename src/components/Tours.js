b1016951b98bfe84f8e152e5a111768088880fbf
BOTSHIVAM <shivamshibu07@gmail.com>
2023-09-09 13:26:07 +0530

    files added

---
+++ src/components/Tours.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Card from "./Card";
+
+
+function Tours({tours, removeTour }){
+return(
+<div>
+    <div className="container">
+    <h2 className="title">TRIP PLANNER</h2>
+    </div>
+    <div className="cards">
+    {
+        tours.map( (tour) => {
+            return <Card key={tour.id} {...tour} removeTour = {removeTour}></Card>
+        })
+    }
+
+    </div>
+
+
+
+</div>
+);
+}
+
+export default Tours;
